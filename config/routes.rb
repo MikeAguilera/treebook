73bb010c9147283c6133c6e69dd0477a238b5ccc
Michael Aguilera <michael.aguilera@gmail.com>
2014-01-17 16:18:04 -0800

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Treebook::Application.routes.draw do
   resources :statuses
-
+root :to => "statuses#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +55,6 @@ Treebook::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+
 end
